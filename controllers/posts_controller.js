64fb0277e6bde34ae26537566076251e8c4c1ada
NikhilJi143 <137680022+NikhilJi143@users.noreply.github.com>
2023-08-16 12:06:39 +0530

    CODEIAL

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -7,7 +7,11 @@ module.exports.create = async function(req, res){
             content: req.body.content,
             user: req.user._id
         });
-        if(req.xhr){
+        
+        if (req.xhr){
+            // if we want to populate just the name of the user (we'll not want to send the password in the API), this is how we do it!
+            post = await post.populate('user', 'name').execPopulate();
+
             return res.status(200).json({
                 data: {
                     post: post
@@ -21,6 +25,8 @@ module.exports.create = async function(req, res){
 
     }catch(err){
         req.flash('error', err);
+        // added this to view the error on console as well
+        console.log(err);
         return res.redirect('back');
     }
   
@@ -36,9 +42,19 @@ module.exports.destroy = async function(req, res){
             post.remove();
 
             await Comment.deleteMany({post: req.params.id});
-          
+
+
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
             req.flash('success', 'Post and associated comments deleted!');
-           
+
             return res.redirect('back');
         }else{
             req.flash('error', 'You cannot delete this post!');
@@ -47,7 +63,7 @@ module.exports.destroy = async function(req, res){
 
     }catch(err){
         req.flash('error', err);
-        return;
+        return res.redirect('back');
     }
     
 }
